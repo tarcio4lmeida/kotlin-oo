79681b3d41712a8e4c12052c8e3794f03ec379ed
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-11 15:11:28 -0300

    Restricoes das variaveis val e var

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
- fun main(){
-   print("Bem vindo ao Bytebank")
- }
+fun main(){
+    println("Bem vindo ao Bytebank")
+    val titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldo")
+}
