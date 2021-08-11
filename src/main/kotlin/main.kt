48f8a474f07d3bdbfc447d520332d408442de800
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-11 15:18:02 -0300

    Estruturas condicionais

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,4 +9,19 @@ fun main(){
     println("titular $titular")
     println("numero da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if(saldo > 0.0){
+        println("conta é positiva")
+    } else if (saldo == 0.0){
+        println("conta é neutra")
+    } else {
+         println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else ->  println("conta é negativa")
+    }
+
 }
