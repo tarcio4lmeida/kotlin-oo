77c91c02ec0201bbfc4b45e99eaf2e6f92a4c18e
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-11 15:45:27 -0300

    Criando classes e objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,66 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular = "Alex"
-    val numeroConta = 1000
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+}
+
+class Conta{
+    var titular = ""
+    var numero = 0
     var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+}
 
-    println("titular $titular")
-    println("numero da conta $numeroConta")
-    println("saldo da conta $saldo")
+fun testaLacos(){
+    for (i in 5 downTo 1 step 2) {
+        if(i == 4){
+            break;
+        }
 
-    if(saldo > 0.0){
+        val titular = "Alex $i"
+        val numeroConta = 1000 +i
+        var saldo = 0.0 + i
+        saldo = 100 + 2.0
+        saldo += 200
+
+        println("titular $titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+
+        println()
+
+
+
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0) {
         println("conta é positiva")
-    } else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("conta é neutra")
     } else {
-         println("conta é negativa")
+        println("conta é negativa")
     }
 
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("conta é neutra")
-        else ->  println("conta é negativa")
+        else -> println("conta é negativa")
     }
-
 }
