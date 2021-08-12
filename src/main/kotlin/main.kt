e8473387a153c8796dbd1dffa1133e0aded25de4
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-12 16:21:22 -0300

    Utilizando interfaces

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
-
-    testaAutenticacao()
+    val cliente = Cliente(
+        nome = "Gui",
+        cpf= "000.00.000-11",
+        senha = 123456
+    )
+    val sistema = SistemaInterno()
+    sistema.entra(admin = cliente, senha = 123456)
 }
 
 
