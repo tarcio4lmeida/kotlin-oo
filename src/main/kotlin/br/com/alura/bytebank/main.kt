dfccc66b545bb326bd43324dfe89bbe1d195fbec
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-16 11:44:40 -0300

    Utilizando variaveis globais

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,18 +1,19 @@
 import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.SistemaInterno
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
 
 fun main() {
     println("Bem vindo ao Bytebank")
 
     val cliente = Cliente(
         nome = "Gui",
-        cpf= "000.00.000-11",
+        cpf = "000.00.000-11",
         senha = 123456
     )
-    val sistema = SistemaInterno()
-    sistema.entra(admin = cliente, senha = 123456)
-
+    val contaPoupanca = ContaPoupanca(titular = cliente, numero = 1000)
     testaContasDiferentes()
+
+    println("Total de contas: ${totalContas}")
 }
 
 
--- src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,11 +1,18 @@
 package br.com.alura.bytebank.modelo
 
+var totalContas: Int = 0
+    private set
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
     ) {
     var saldo = 0.0
         protected set
+
+    init {
+        println("criando conta")
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         this.saldo += valor
@@ -21,4 +28,35 @@ abstract class Conta(
         }
        return false
     }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor  + 0.1
+        if (this.saldo >= valorComTaxa){
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor){
+            this.saldo -= valor
+        }
+    }
+
 }
