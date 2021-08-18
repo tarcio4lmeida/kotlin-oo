c2b56ff565ba5ff37affd11bfb41e283befcc22c
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-18 15:14:32 -0300

    Safe call

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,13 @@
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    var enderecoNaoNulo : Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    println(enderecoNulo?.logradouro?.length)
+
+    enderecoNulo = null
+    enderecoNulo?.let {
+        endereco : Endereco -> println(endereco.logradouro.length)
+    }
 }
 
 
